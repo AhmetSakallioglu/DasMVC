d801c507bb33735113fe2e415ab43fc7bb92e858
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2023-12-12 14:02:07 +0300

    Add - Account Controller and Login Page

--- dasMVC/obj/Debug/net6.0/dasMVC.AssemblyInfo.cs
+++ dasMVC/obj/Debug/net6.0/dasMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef97561cb3c9cbb4588f76f99346ff9ee12b0756")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("dasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
