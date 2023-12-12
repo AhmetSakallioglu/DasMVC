412644ddc0da120e7903dbd6f8bea5dd30efef9d
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2023-12-12 12:55:58 +0300

    Add Razor Runtime

--- dasMVC/Program.cs
+++ dasMVC/Program.cs
@@ -7,7 +7,7 @@ namespace dasMVC
 			var builder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+			builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 			var app = builder.Build();
 
