25a20b9f056bbf0e8dcb531a09f0deb89c4c634d
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2023-12-28 19:41:56 +0300

    More frontend

---
+++ dasMVC/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dasMVC.Controllers
+{
+    [Authorize]
+    public class ContactController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
