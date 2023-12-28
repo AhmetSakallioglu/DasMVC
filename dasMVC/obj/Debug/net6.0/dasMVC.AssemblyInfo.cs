b96e154464537d8bf726324a4d6933adc8b3d7a9
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2023-12-28 20:23:07 +0300

    Update - Login Page

--- dasMVC/obj/Debug/net6.0/dasMVC.AssemblyInfo.cs
+++ dasMVC/obj/Debug/net6.0/dasMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dasMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a20b9f056bbf0e8dcb531a09f0deb89c4c634d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dasMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dasMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
