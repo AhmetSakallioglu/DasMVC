b38e9ff0147faf87770f94a3f9fea2916f4d9fc3
Ahmet Sakallıoğlu <ahmetsakallioglu54@gmail.com>
2023-12-29 17:41:20 +0300

    Add Localization

--- dasMVC/Controllers/HomeController.cs
+++ dasMVC/Controllers/HomeController.cs
@@ -1,17 +1,28 @@
 ﻿using dasMVC.Models;
+using dasMVC.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace dasMVC.Controllers
 {
+
 	[Authorize]
 	public class HomeController : Controller
 	{
+        private LanguageService _localization;
 
-		[AllowAnonymous]
+        public HomeController(LanguageService localization)
+        {
+            _localization = localization;
+        }
+
+        [AllowAnonymous]
 		public IActionResult Index()
 		{
+			ViewBag.Home = _localization.Getkey("Welcome").Value;
+			var currentCulture = Thread.CurrentThread.CurrentCulture.Name;
 			return View();
 		}
 
@@ -33,5 +44,17 @@ namespace dasMVC.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-	}
+
+
+        [AllowAnonymous]
+        public IActionResult ChangeLanguage(string culture)
+        {
+			Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+				CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)), new CookieOptions()
+				{
+					Expires = DateTimeOffset.UtcNow.AddYears(1)
+				});
+            return Redirect(Request.Headers["Referer"].ToString ());
+        }
+    }
 }
--- dasMVC/Program.cs
+++ dasMVC/Program.cs
@@ -1,6 +1,10 @@
 using dasMVC.Entities;
+using dasMVC.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Globalization;
 using System.Reflection;
 
 namespace dasMVC
@@ -10,6 +14,31 @@ namespace dasMVC
         public static void Main(string[] args)
         {
 			var builder = WebApplication.CreateBuilder(args);
+
+            #region Localizer
+            builder.Services.AddSingleton<LanguageService>();
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+            builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
+            options.DataAnnotationLocalizerProvider = (type, factory) =>
+            {
+                var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+                return factory.Create(nameof(SharedResource), assemblyName.Name);
+            });
+
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("tr-TR"),
+                };
+                options.DefaultRequestCulture = new RequestCulture(culture: "tr-TR", uiCulture: "tr-TR");
+                options.SupportedCultures = supportCultures;
+                options.SupportedUICultures = supportCultures;
+                options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+            });
+            #endregion
+
 
             // Add services to the container.
             builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
@@ -46,6 +75,7 @@ namespace dasMVC
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
             app.UseRouting();
 
             app.UseAuthentication();
